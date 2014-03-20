d861fa78aa87d1158f372002d1397aec936b8b27
so <soso2501@gmail.com>
2014-03-21 01:08:09 +0800

    增加reids类和log类，并增加配置文件

---
+++ application/library/DB/Redis.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * 封装的操作redis的类，支持主服务器和从服务器
+ * 支持选择数据库
+ * 如果配置了主库和从库，那么默认都从主库操作，不管读写
+ * 可以调用函数来指定操作的主从库
+ * 可以把host配置成一个数组，那么会随机从中抽出一台来操作
+ * 目前还不支持权重
+ *
+ * $config 用来传redis服务器的参数
+ * $config = array(
+ *  "master" => array(
+ *      "host" => "127.0.0.1",
+ *      "port" => "6379",
+ *      "db" => 1,
+ *  ),
+ *  "slave" => array(
+ *      "host" => "127.0.0.1",
+ *      "port" => "6380",
+ *      "db" => 1,
+ *  ),
+ * )
+ */
+
+class DB_Redis{
+    //redis的服务器配置
+    private $config;
+
+    //是操作主库还是从库
+    private $queryType;
+
+    //连接缓存
+    private $links;
+
+    //超时时间，默认为2s
+    private $timeout = 2;
+
+    public function __construct($config){
+        if(!empty($config['master'])){
+            $this->queryType = "master";
+        }else{
+            $this->queryType = "slave";
+        }
+        $this->config = $config;
+    }
+
+    //设置操作主库还是从库，如果与配置不符合，将返回false
+    public function setQueryType($type){
+        if(empty($this->config[$type])){
+            return false;
+        }
+        $this->queryType = $type;
+        return true;
+    }
+
+    //把这个函数抽象出来，为了以后改配置结构的时候可以更独立点
+    private function _connect($single_config){
+        if(is_array($single_config['host'])){
+            $host = $single_config['host'][array_rand($single_config['host'])];
+        }else {
+            $host = $single_config['host'];
+        }
+        $port = empty($single_config['port']) ? 6379 : $single_config['port'];
+        $timeout = empty($single_config['timeout']) ? $this->timeout : $single_config['timeout'];
+        if(empty($single_config['host'])){
+            ToolLog::log("Redis Config Error, Not Find Host", 2);
+            return false;
+        }
+        $this->links[$this->queryType] = $link = new Redis();
+        $succ = $link->connect($host, $port, $timeout);
+        if(!$succ){
+            ToolLog::log("Redis Connect Failed", 2);
+            return false;
+        }
+        if($single_config['db']){
+            $link->select($single_config['db']);
+        }
+        return true;
+    }
+
+    private function _getLink(){
+        if(empty($this->links[$this->queryType])){
+            if(empty($this->config[$this->queryType])){
+                ToolLog::log("Redis Config Error, Not Find QueryType " . $this->queryType, 2);
+                return false;
+            }
+            $succ = self::_connect($this->config[$this->queryType]);
+            if(!$succ)
+                return false;
+        }
+        return $this->links[$this->queryType];
+    }
+
+    //所有实质性的操作函数都会调用到这里来
+    public function __call($method, $params){
+        //不允许操作的命令
+        $denyMethod = array('setoption', 'bgrewriteaof', 'bgsave', 'flushdb', 'flushall');
+        if(in_array(strtolower($method), $denyMethod)){
+            ToolLog::log("Redis Deny Operator $method!");
+            return false;
+        }
+        $link = self::_getLink();
+        if($link){
+            return call_user_func_array(array($link, $method), $params);
+        }else return false;
+    }
+    
+}
---
+++ application/library/Tool/Log.php
@@ -0,0 +1,65 @@
+<?php
+class Tool_Log{
+    //如果是记录在文件里，则logfile会是对应的文件名，如果为空。表示直接输出
+    private static $logfile = "";
+    //多少错误等级的错误需要记录
+    private static $loglevel = 1;
+
+    //加载日志记录的配置文件
+    private static function _loadConfig(){
+        //是否已经load过配置文件了
+        static $hasload = false;
+        if($hasload) return;
+
+        $config = new Yaf_Config_Ini(APP_PATH . "/conf/log.ini", 'product');
+        self::$logfile = $config->logfile;
+        self::$loglevel = empty($config->loglevel) ? 1 : intval($config->loglevel);
+    }
+
+    /**
+     * 记录日志，原样记录信息msg，并判断level值是否是需要记录的范围
+     */
+    public static function log($msg, $level = 1){
+        self::_loadConfig();
+
+        if($level < self::$loglevel) return;
+        if(empty(self::$logfile)){
+            echo "<pre>ERROR_REPORTING:LEVEL $level\n";
+            echo $msg . "\n";
+            print_r(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
+            echo "</pre>";
+        }else{
+            $line = "[" . date("Y-m-d H:i:s") . "]" . $msg . json_encode(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)) . "\n";
+            self::logToFile($line, self::$logfile);
+        }
+    }
+
+    //把line写入到file文件中
+    public static function logToFile($line, $file){
+        //有些服务器的权限有问题
+        $oldmask = umask(0);
+        self::makeDir(dirname($file));
+        
+        $fp = fopen($file, "ab");
+        if($fp){
+            fwrite($fp, $line);
+            fclose($fp);
+            umask($oldmask);
+            return true;
+        }else{
+            umask($oldmask);
+            return false;
+        }
+    }
+
+    //创建目录函数
+    public static function makeDir($dir){    
+        if(empty($dir)) return false;
+        if(file_exists($dir))  return true;
+
+        $oldmask = umask(0);
+        $return = mkdir($dir, 0755, true);
+        umask($oldmask);
+        return $return;
+    }                      
+}
