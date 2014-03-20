7aba0bd6ce0de5e5e373962aea32b8216f1cd8e9
so <soso2501@gmail.com>
2014-03-21 07:40:56 +0800

    对git命令不理解，没有提交

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -1,5 +1,13 @@
 <?php
 class PageController extends Yaf_Controller_Abstract{
     public function indexAction(){
+        //用来在模板里判断高亮显示哪个栏
+		$this->getView()->assign("page_type", "page_partner");
+
+        //初使化redis配置
+        $config = new Yaf_Config_Ini(APP_PATH . "/conf/source.ini", "product");
+        $redis = new DB_Redis($config->redis);
+        //var_dump($r->set("a", "BBBB"), $config->redis);die;
+
     }
 }
